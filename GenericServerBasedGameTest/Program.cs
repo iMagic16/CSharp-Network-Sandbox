8e5889aed67e368cd5cba736782092e8273ca099
James W <jameswalsh5683@gmail.com>
2016-05-05 01:12:59 +0100

    Client/Server integration. MK1. Initial Commit

---
+++ GenericServerBasedGameTest/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NetworkCommsDotNet;
+using NetworkCommsDotNet.Connections;
+using System.Threading;
+
+namespace GenericServerBasedGameTest_Client
+{
+    static class Program
+    {
+        static string serverInfo, serverIP, MsgToSend, MsgType;
+        static int serverPort;
+
+        static void Main()
+        {
+
+            Console.WriteLine(">Starting Client");
+            serverInfo = "192.168.0.10:7700"; //server IP:Port
+            Console.WriteLine(">>serverInfo = {0}", serverInfo);
+            Console.WriteLine(">>Splitting into seperate vars");
+            GetServerIPAndPort(serverInfo);
+            SetupListener();
+
+            MsgToSend = "";
+            SendMsg(serverIP, serverPort, MsgType, MsgToSend);
+
+            // ShutdownConnection();
+
+            Console.ReadLine();
+        }
+
+
+        static void ParseReceivedMsg(string MessageType, string Message)
+        {
+            Console.WriteLine("PARSING: " + MessageType + ": " + Message);
+
+
+            switch (MessageType)
+            {
+                case "LoginReq":
+                    Console.WriteLine(MessageType);
+                    break;
+
+                case "Message":
+                    Console.WriteLine(MessageType);
+                    break;
+
+                default:
+                    Console.WriteLine("Message Type Unknown");
+                    break;
+            }
+
+
+
+
+            Console.ReadLine();
+
+
+        }
+
+        static void SendMsg(string IP, int PORT, string MSGTYPE, string MSG)
+        {
+            try
+            {
+                Console.WriteLine(">Sending MSG '" + MSG + "'");
+                NetworkComms.SendObject(MSGTYPE, IP, PORT, MSG);
+                Console.WriteLine(">Sent!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+
+        }
+
+        static void ShutdownConnection()
+        {
+            NetworkComms.Shutdown();
+        }
+
+        static void GetServerIPAndPort(string serverInformation)
+        {
+            serverIP = serverInformation.Split(':').First();
+            Console.WriteLine(">>>IP: {0}", serverIP);
+            serverPort = Convert.ToInt32(serverInformation.Split(':').Last());
+            Console.WriteLine(">>>Port: {0}", serverPort);
+
+        }
+
+
+
+        static void SetupListener()
+        {
+
+            NetworkComms.AppendGlobalIncomingPacketHandler<string>("Message", PrintIncomingMessage);
+            Connection.StartListening(ConnectionType.TCP, new IPEndPoint(IPAddress.Any, 7701));
+
+        }
+
+
+        private static void PrintIncomingMessage(PacketHeader header, Connection connection, string message)
+        {
+            Console.WriteLine(">>>>(Client) A message was received from " + connection.ToString() + " which said '" + message + "'.");
+            ParseReceivedMsg(header.PacketType, message.ToString());
+
+            //   string ClientIP = Convert.ToString(((System.Net.IPEndPoint)connection.ConnectionInfo.RemoteEndPoint).Address);
+            //  string ClientPORT = Convert.ToString(((System.Net.IPEndPoint)connection.ConnectionInfo.RemoteEndPoint).Port);
+
+            //  MsgToSend = message + "ACK'd";
+
+            // SendMsg(ClientIP, Convert.ToInt32(ClientPORT), MsgToSend);
+        }
+
+
+    }
+}
---
+++ GenericServerBasedGameTest_Server/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using NetworkCommsDotNet;
+using NetworkCommsDotNet.Connections;
+namespace GenericServerBasedGameTest_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Trigger the method PrintIncomingMessage when a packet of type 'Message' is received
+            //We expect the incoming object to be a string which we state explicitly by using <string>
+            NetworkComms.AppendGlobalIncomingPacketHandler<string>("Message", PrintIncomingMessage);
+            //Start listening for incoming connections
+            Connection.StartListening(ConnectionType.TCP, new IPEndPoint(IPAddress.Any, 7700));
+
+            //Print out the IPs and ports we are now listening on
+            Console.WriteLine(">Server listening for TCP connection on:");
+            foreach (IPEndPoint localEndPoint in Connection.ExistingLocalListenEndPoints(ConnectionType.TCP))
+                Console.WriteLine(">>{0}:{1}", localEndPoint.Address, localEndPoint.Port);
+
+            //Let the user close the server
+            Console.WriteLine(">Press any key to close server.");
+            Console.ReadKey(true);
+
+            //We have used NetworkComms so we should ensure that we correctly call shutdown
+            NetworkComms.Shutdown();
+        }
+
+        /// <summary>
+        /// Writes the provided message to the console window
+        /// </summary>
+        /// <param name="header">The packet header associated with the incoming message</param>
+        /// <param name="connection">The connection used by the incoming message</param>
+        /// <param name="message">The message to be printed to the console</param>
+        private static void PrintIncomingMessage(PacketHeader header, Connection connection, string message)
+        {
+            Console.WriteLine(">>>>(Server) A message was received from " + connection.ToString() + " which said '" + message + "'.");
+
+        //    Console.WriteLine(connection.ConnectionInfo.RemoteEndPoint);
+
+            string ClientIP = Convert.ToString(((System.Net.IPEndPoint)connection.ConnectionInfo.RemoteEndPoint).Address);
+
+            string ClientPORT = Convert.ToString(((System.Net.IPEndPoint)connection.ConnectionInfo.RemoteEndPoint).Port);
+
+            string MsgToSend = "ACK";
+
+            SendMsg(ClientIP, Convert.ToInt32(ClientPORT), MsgToSend);
+        }
+
+
+        static void SendMsg(string IP, int PORT, string MSG)
+        {
+            try
+            {
+                Console.WriteLine(">Sending MSG '" + MSG + "'");
+                NetworkComms.SendObject("Message", IP, PORT, MSG);
+                Console.WriteLine(">Sent!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+
+        }
+
+        static void ShutdownConnection()
+        {
+            NetworkComms.Shutdown();
+        }
+
+
+    }
+
+}
