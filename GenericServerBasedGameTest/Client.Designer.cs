60d89f791be6d4d76309f644a63371bf31b3813c
James W <jameswalsh5683@gmail.com>
2016-07-25 11:17:23 +0100

    Form Layout

--- GenericServerBasedGameTest/Client.Designer.cs
+++ GenericServerBasedGameTest/Client.Designer.cs
@@ -28,11 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BtnSend = new System.Windows.Forms.Button();
+            this.TxtCmdToSend = new System.Windows.Forms.TextBox();
+            this.TxtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnSend
+            // 
+            this.BtnSend.Location = new System.Drawing.Point(249, 233);
+            this.BtnSend.Name = "BtnSend";
+            this.BtnSend.Size = new System.Drawing.Size(23, 20);
+            this.BtnSend.TabIndex = 5;
+            this.BtnSend.UseVisualStyleBackColor = true;
+            // 
+            // TxtCmdToSend
+            // 
+            this.TxtCmdToSend.Location = new System.Drawing.Point(12, 233);
+            this.TxtCmdToSend.Name = "TxtCmdToSend";
+            this.TxtCmdToSend.Size = new System.Drawing.Size(230, 20);
+            this.TxtCmdToSend.TabIndex = 4;
+            // 
+            // TxtOutput
+            // 
+            this.TxtOutput.Location = new System.Drawing.Point(13, 8);
+            this.TxtOutput.Multiline = true;
+            this.TxtOutput.Name = "TxtOutput";
+            this.TxtOutput.ReadOnly = true;
+            this.TxtOutput.Size = new System.Drawing.Size(259, 219);
+            this.TxtOutput.TabIndex = 3;
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.BtnSend);
+            this.Controls.Add(this.TxtCmdToSend);
+            this.Controls.Add(this.TxtOutput);
+            this.Name = "Client";
             this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnSend;
+        private System.Windows.Forms.TextBox TxtCmdToSend;
+        private System.Windows.Forms.TextBox TxtOutput;
     }
 }
--- GenericServerBasedGameTest_Server/Server.Designer.cs
+++ GenericServerBasedGameTest_Server/Server.Designer.cs
@@ -28,11 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.TxtOutput = new System.Windows.Forms.TextBox();
+            this.TxtCmdToSend = new System.Windows.Forms.TextBox();
+            this.BtnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtOutput
+            // 
+            this.TxtOutput.Location = new System.Drawing.Point(13, 13);
+            this.TxtOutput.Multiline = true;
+            this.TxtOutput.Name = "TxtOutput";
+            this.TxtOutput.ReadOnly = true;
+            this.TxtOutput.Size = new System.Drawing.Size(259, 219);
+            this.TxtOutput.TabIndex = 0;
+            // 
+            // TxtCmdToSend
+            // 
+            this.TxtCmdToSend.Location = new System.Drawing.Point(12, 238);
+            this.TxtCmdToSend.Name = "TxtCmdToSend";
+            this.TxtCmdToSend.Size = new System.Drawing.Size(230, 20);
+            this.TxtCmdToSend.TabIndex = 1;
+            // 
+            // BtnSend
+            // 
+            this.BtnSend.Location = new System.Drawing.Point(249, 238);
+            this.BtnSend.Name = "BtnSend";
+            this.BtnSend.Size = new System.Drawing.Size(23, 20);
+            this.BtnSend.TabIndex = 2;
+            this.BtnSend.UseVisualStyleBackColor = true;
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 268);
+            this.Controls.Add(this.BtnSend);
+            this.Controls.Add(this.TxtCmdToSend);
+            this.Controls.Add(this.TxtOutput);
+            this.Name = "Server";
             this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox TxtOutput;
+        private System.Windows.Forms.TextBox TxtCmdToSend;
+        private System.Windows.Forms.Button BtnSend;
     }
 }
